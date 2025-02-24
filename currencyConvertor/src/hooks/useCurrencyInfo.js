644b61f927a68aa246fbb17d406371571ed1e5b9
Pushpraj Desai <106546262+Pushpraj1522002@users.noreply.github.com>
2025-02-24 10:55:24 +0530

    Add files via upload

---
+++ currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,21 @@
+//this is a Costom Hook
+//we can build Costom Hooks with or without the help of buildin hooks.
+
+import {useEffect, useState} from 'react'
+
+function useCurrencyInfo(currency){
+    
+    const [data, setData] = useState({})
+
+    useEffect(()=>{
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+        .then((res)=>res.json())
+        .then((res)=>setData(res[currency]))
+        console.log(data);
+    },[currency])
+
+    console.log(data);
+    return data;
+}
+
+export default useCurrencyInfo
